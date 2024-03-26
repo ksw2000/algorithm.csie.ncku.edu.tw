467ceb1e847b38a3b7f9cf27bc07de35ba63971c
Kashiwa <liao2000@livemail.tw>
2024-03-26 22:47:35 +0800

    Close side bar when NavigationEnd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,20 +9,31 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-
 export class AppComponent {
-  openSideBar() {
-    console.log("opensidebar()");
-    const menuBtn = document.querySelector('#button-less-900px a i');
-    const sideBar = document.getElementById('button-over-900px');
-    if (sideBar !== null && menuBtn !== null) {
-      if (sideBar.classList.contains('show')) {
-        sideBar.classList.remove('show');
-        menuBtn.innerHTML = 'menu';
-      } else {
-        sideBar.classList.add('show');
-        menuBtn.innerHTML = 'close';
+  constructor(private router: Router) { }
+
+  toggleSideBar(forceClose: boolean = false) {
+    if(document){
+      const menuBtn = document.querySelector('#button-less-900px a i');
+      const sideBar = document.getElementById('button-over-900px');
+      if (sideBar !== null && menuBtn !== null) {
+        if (forceClose || sideBar.classList.contains('show')) {
+          sideBar.classList.remove('show');
+          menuBtn.innerHTML = 'menu';
+        } else {
+          sideBar.classList.add('show');
+          menuBtn.innerHTML = 'close';
+        }
       }
     }
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.toggleSideBar(true);
+        console.log('NavigationEnd event:', event);
+      }
+    });
   }
 }
